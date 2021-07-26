b7a07cff46997000592933c13823062082cc85a3
Rian Ribeiro Rodrigues <rianribeiroprogramador@gmail.com>
2021-07-26 18:34:39 -0300

    rocket.q

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,22 +1,20 @@
 export default function Modal() {
+  const modalWrapper = document.querySelector(".modal-wrapper");
+  const cancelButton = document.querySelector(".button.cancel");
 
-  const modalWrapper = document.querySelector('.modal-wrapper')
-  const cancelButton = document.querySelector('.button.cancel')
-
-  cancelButton.addEventListener('click', close)
+  cancelButton.addEventListener("click", close);
 
   function open() {
     // Atribuir a classe active para a modal.
-    modalWrapper.classList.add('active')
+    modalWrapper.classList.add("active");
   }
   function close() {
     // Remover a classe active para a modal.
-    modalWrapper.classList.remove('active')
+    modalWrapper.classList.remove("active");
   }
 
   return {
     open,
-    close
-  }
-
+    close,
+  };
 }
